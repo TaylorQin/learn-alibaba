62261217ab7b8be36e276aeb70dacfe26fb97799
TaylorQ <xiaotao.qin@qq.com>
2020-04-13 09:49:36 +0800

    cahnge package com.learn to com.taylor

--- nacos-config-client-load/src/main/java/com/taylor/config/NacosConfigClientLoadApplication.java
+++ nacos-config-client-load/src/main/java/com/taylor/config/NacosConfigClientLoadApplication.java
@@ -1,4 +1,4 @@
-package com.learn.config;
+package com.taylor.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- nacos-config-client-load/src/main/java/com/taylor/config/controller/ConfigClientController.java
+++ nacos-config-client-load/src/main/java/com/taylor/config/controller/ConfigClientController.java
@@ -1,4 +1,4 @@
-package com.learn.config.controller;
+package com.taylor.config.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
