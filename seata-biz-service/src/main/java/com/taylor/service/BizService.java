fdb13e0fe7ad5b530c787f4896150fab09fb67e2
TaylorQ <xiaotao.qin@qq.com>
2020-05-20 15:03:07 +0800

    add spring profiles support for nacos-discovery-consumer and nacos-discovery-provider

--- seata-biz-service/src/main/java/com/taylor/service/BizService.java
+++ seata-biz-service/src/main/java/com/taylor/service/BizService.java
@@ -1,5 +1,6 @@
 package com.taylor.service;
 
+import io.seata.spring.annotation.GlobalTransactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,11 +12,12 @@ public class BizService {
     @Autowired
     RestTemplate restTemplate;
 
+    @GlobalTransactional
     @Transactional
     public void biz() {
         restTemplate.getForObject("http://localhost:8020/order", String.class);
         restTemplate.getForObject("http://localhost:8030/pay", String.class);
-        int i = 1/0;
+//        int i = 1/0;
     }
 
 }
--- seata-order-service/src/main/java/com/taylor/SeataOrderSercviceApplication.java
+++ seata-order-service/src/main/java/com/taylor/SeataOrderSercviceApplication.java
@@ -1,7 +1,12 @@
 package com.taylor;
 
+import io.seata.rm.datasource.DataSourceProxy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class SeataOrderSercviceApplication {
